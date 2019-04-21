69e66d35f04c636733688dd824143b92a2be36c8
Carl Cortright <carl@carlcortright.com>
2019-04-21 08:39:46 -0600

    live updates working on class page

--- src/scenes/ClassroomView.js
+++ src/scenes/ClassroomView.js
@@ -1,23 +1,109 @@
 import React, { Component } from 'react';
-import { GetClass } from '../api/APIHelpers'
+import { Heading, Card, Flex } from 'rebass';
+import { GetClass, GetLatestQuestions } from '../api/APIHelpers';
+import styled from 'styled-components'
 
 class ClassroomView extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            phone_number: null,
+            questions: [],
+            classname: '',
+            last_question: 0
+        }
+    }
+
     componentDidMount () {
+        let self = this;
         const { classroom } = this.props.match.params
-        console.log(classroom)
         let response = GetClass(classroom)
         response.then(function(result) {
-            console.log(result);
+            // Make the call to get the initial classroom data
+            let last_index = result.data.questions.length - 1;
+            self.setState({
+                phone_number: result.data.phone_number,
+                questions: result.data.questions,
+                last_question: result.data.questions[last_index].id,
+                classname: classroom
+            })
+            // Start polling to get all new questions
+            self.timer = setInterval(() => self.pollForQuestions(), 500);
         })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    pollForQuestions = () => {
+        let self = this;
+        let response = GetLatestQuestions(this.state.classname, this.state.last_question)
+        response.then(function(result){
+            let data = result.data;
+            if (data.length > 0){
+                self.setState({
+                    questions: self.state.questions.concat(data),
+                    last_question: data[data.length-1].id
+                })
+            }
+        })
+    }
+
+    formatPhoneNumber = (number_string) => {
+        var cleaned = ('' + number_string).replace(/\D/g, '')
+        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
+        if (match) {
+            var intlCode = (match[1] ? '+1 ' : '')
+            return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('')
+        }
+        return null
+    }
+
+    createQuestions = (questions) => {
+        let cards = []
+        for (let i = questions.length-1; i >= 0; i--) {
+            let content = questions[i].content
+            cards.push(
+                <Card
+                    fontSize={3}
+                    fontWeight='bold'
+                    width={[ 1, 1, 1]}
+                    p={3}
+                    my={3}
+                    boxShadow='0 2px 16px rgba(0, 0, 0, 0.25)'
+                >
+                    {content}
+                </Card>
+            )
+
+        }
+        return cards
     }
 
     render() {
         return (
         <div>
-            <p>Classroom Question View</p>
+            <Heading 
+                textAlign='center'
+                fontSize={[ 3, 4, 5, 6 ]}
+                p={4}
+            >
+                {this.formatPhoneNumber(this.state.phone_number)}
+            </Heading>
+            <Flex justifyContent='center' flexWrap='wrap'>
+                <QuestionList>
+                    {this.createQuestions(this.state.questions)}
+                </QuestionList>
+            </Flex>
         </div>
         );
     }
 }
+
+const QuestionList = styled.div`
+    flex: 0 1 800px;
+`
 
 export default ClassroomView;
