482d64853c2d2e9a3ad0ab9e597f23567d6f0ebf
Carl Cortright <carl@carlcortright.com>
2019-04-21 09:25:24 -0600

    styled homepage

--- src/scenes/CreateClassroom.js
+++ src/scenes/CreateClassroom.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Flex } from 'rebass';
+import { Button, Flex, Heading } from 'rebass';
 import styled from 'styled-components'
 
 class CreateClassroom extends Component {
+
+
   render() {
     return (
         <TallFlex
@@ -10,20 +12,47 @@ class CreateClassroom extends Component {
         justifyContent='center'
         flexWrap='wrap'
         >
-            <ClassInput></ClassInput>
-            <Button bg='#246068'>Create Classroom</Button>
+            <CreateDiv>
+                <Heading 
+                    textAlign='center'
+                    fontSize={[ 6, 8 ]}
+                    p={4}
+                >
+                    ClassQA
+                </Heading>
+                <ClassInput placeholder='Classroom Name'></ClassInput>
+                <Button 
+                    bg='#246068'
+                    my={3}
+                    alignSelf='center'
+                    fontSize={4}
+                >
+                    Create Classroom
+                </Button>
+            </CreateDiv>
+            
         </TallFlex>
     );
   }
 }
 
 const TallFlex = styled(Flex)`
-  height: 100vh;
+  height: calc(100vh - 60px);
+`
+
+const CreateDiv = styled.div`
+    flex: 0 1 600px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 `
 
 const ClassInput = styled.input`
-    width: 400px;
-    flex: 0 0 100%;
+    width: 100%;
+    font-size: 22px;
+    border-radius: 5px;
+    border: 2px solid #ccc;
+    padding: 3px;
 `
 
 export default CreateClassroom;
